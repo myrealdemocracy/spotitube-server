f5d46da6b1f653e1bc4d41403de75c3a5481e56e
Mark Oude Veldhuis <mark.oudeveldhuis@nedap.com>
2013-09-25 23:49:12 +0200

    Inherit the new secret key base from Rails 4

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SpotitubeServer::Application.config.secret_token = '5908dfe85ab9820f3ea3c50343159086f78ce6c1a1eabcb9ace7a7f36cdf45cc8aa081278ababf4b31ef036985a84085667ac73c5f2480c91218c04c0050a095'
